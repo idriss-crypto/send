d96ab5625d99c9d1af095839097dff7a46467baa
idrisssystem <idrisssystem@gmail.com>
2022-08-11 23:32:32 +0200

    local import core & nfts loading fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {
     SendToAnyoneMain,
     SendToAnyoneAddress
 } from "@idriss-crypto/send-to-anyone-core/subpages";
-let addressNFTs
+
 document.addEventListener('DOMContentLoaded', async () => {
     const sendToAnyoneLogicPromise = await import ("@idriss-crypto/send-to-anyone-core/sendToAnyoneLogic")
     const getProviderPromise = import("@idriss-crypto/send-to-anyone-core/getWeb3Provider")
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         console.log(SendToAnyoneLogic.web3)
         const accounts = await SendToAnyoneLogic.web3.eth.getAccounts();
         let selectedAccount = accounts[0];
-        addressNFTs = getNFTsForAddress(selectedAccount, ALCHEMY_API_KEY)
+        const addressNFTs = getNFTsForAddress(selectedAccount, ALCHEMY_API_KEY)
         //TODO: end ^^^^^
 
         if (!identifier || !recipient) {
