99ea52b602f8c81d97d11ba49d3e0f0667d34cac
lennardevertz <levertz@idriss.xyz>
2023-06-22 19:07:44 +0200

    WC migration attempt

--- src/generateSendToAnyoneCode.js
+++ src/generateSendToAnyoneCode.js
@@ -1,7 +1,7 @@
 import css from "!!style-loader!css-loader!sass-loader!./generateSendToAnyoneCode.scss";
 
 addEventListener('input', e => {
-    let link = 'https://idriss.xyz/sendToAnyone';
+    let link = 'https://idriss.xyz/send';
     let params = {};
     if (document.querySelector('input[name="identifier"]').value) {
         params.identifier = document.querySelector('input[name="identifier"]').value;
@@ -15,7 +15,7 @@ addEventListener('input', e => {
     if (Object.values(params).length) {
         link += '?' + Object.entries(params).map(([k, v]) => encodeURIComponent(k) + '=' + encodeURIComponent(v)).join('&');
     }
-    let html = `<script src="https://idriss.xyz/static/js/idrissSendToAnyoneSDK.js"></script><a href="${link}" onclick='idrissShowSendToAnyonePopup(${JSON.stringify(params)},event)'>Send me crypto</a>`;
+    let html = `<script src="https://idriss.xyz/static/js/send/idrissSendToAnyoneSDK.js"></script><a href="${link}" onclick='idrissShowSendToAnyonePopup(${JSON.stringify(params)},event)'>Send me crypto</a>`;
     document.querySelector('.result.link>div').textContent = link
     document.querySelector('.result.html>div').textContent = html
 })
