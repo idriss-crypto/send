732378d04074b5a72b3f4558b0510e77980d7e66
lennardevertz <levertz@idriss.xyz>
2024-08-22 12:57:18 +0200

    - add nav as param to /send

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/send', methods=["GET"])
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/send", methods=["GET"])
 def submodule_index():
-    return render_template('send-to-anyone-page.html')
+    print("getting page")
+    return render_template("send-to-anyone.html")
 
 
 @app.route("/send/<nav>", methods=["GET"])
-def streamer_obs():
-    return render_template('send-to-anyone-page.html')
+def streamer_obs(nav):
+    return render_template("send-to-anyone.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
